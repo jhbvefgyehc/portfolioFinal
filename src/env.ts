dd2aba8f4766040adf58f1930c9d8cd5c5be1414
Sachin S <sachinsubhash286@gmail.com>
2025-11-19 20:38:50 +0530

    Create env.ts

---
+++ src/env.ts
@@ -0,0 +1,18 @@
+// src/env.ts
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+    NEXTAUTH_SECRET: z.string().min(1),
+    NEXTAUTH_URL: z.string().url().optional(),
+  },
+  client: {},
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+});
